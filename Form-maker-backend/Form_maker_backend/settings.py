0ed2a7790d947a1c6c1f5f1f283b5b0e14811f02
D Gopall Krishna <nikhil0223@gmail.com>
2021-02-18 20:40:50 +0530

    Some db files changed

--- Form-maker-backend/Form_maker_backend/settings.py
+++ Form-maker-backend/Form_maker_backend/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 
-    'formmaker.apps.FormmakerConfig'
+    'formmaker.apps.FormmakerConfig',
 ]
 
 MIDDLEWARE = [
