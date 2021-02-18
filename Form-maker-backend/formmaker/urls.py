13c8d2787a3afe22c0115c25d2250d13ae26d283
D Gopall Krishna <nikhil0223@gmail.com>
2021-02-18 20:40:11 +0530

    Made formmaker api
    (Hopefully it should work)

---
+++ Form-maker-backend/formmaker/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from rest_framework import routers
+from .views import FormCreatedViewset, QuestionListViewset, OptionListViewset
+
+app_name = 'formmaker'
+
+router = routers.DefaultRouter()
+router.register(r'formcreated', FormCreatedViewset)
+router.register(r'questionlist', QuestionListViewset)
+router.register(r'optionlist', OptionListViewset)
+
+
+urlpatterns = [
+    path('form_api/', include(router.urls)), 
+]
--- Form-maker-backend/formmaker/views.py
+++ Form-maker-backend/formmaker/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets, permissions
+from .serializers import FormCreatedSerializer, OptionListSerializer, QuestionListSerializer
+from .models import FormCreated, OptionList, QuestionList
 # Create your views here.
+
+class FormCreatedViewset(viewsets.ModelViewSet):
+    queryset = FormCreated.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = FormCreatedSerializer
+
+class QuestionListViewset(viewsets.ModelViewSet):
+    queryset = QuestionList.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = QuestionListSerializer
+
+
+class OptionListViewset(viewsets.ModelViewSet):
+    queryset = OptionList.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = OptionListSerializer
